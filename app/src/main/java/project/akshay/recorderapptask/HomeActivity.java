084ce770566db86a3334a005482754e3d108759e
Akshay Balaji <akshaybalaji12@gmail.com>
2019-05-18 01:01:57 +0530

    Added TabView

--- app/src/main/java/project/akshay/recorderapptask/HomeActivity.java
+++ app/src/main/java/project/akshay/recorderapptask/HomeActivity.java
@@ -1,13 +1,36 @@
 package project.akshay.recorderapptask;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class HomeActivity extends AppCompatActivity {
+
+    TabAdapter tabAdapter;
+    TabLayout tabLayout;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.app_bar);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
+
+        tabAdapter = new TabAdapter(getSupportFragmentManager());
+        tabAdapter.addFragment(new RecorderFragment(),"Recorder");
+        tabAdapter.addFragment(new ListFragment(), "List");
+
+        viewPager.setAdapter(tabAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
