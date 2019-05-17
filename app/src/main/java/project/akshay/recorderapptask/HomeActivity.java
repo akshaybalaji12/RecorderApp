9a3a8008ae31a0a9fc3f04262ae9326bcebfd99f
Akshay Balaji <akshaybalaji12@gmail.com>
2019-05-18 01:51:47 +0530

    Added permissions to record

--- app/src/main/java/project/akshay/recorderapptask/HomeActivity.java
+++ app/src/main/java/project/akshay/recorderapptask/HomeActivity.java
@@ -1,18 +1,30 @@
 package project.akshay.recorderapptask;
 
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import java.util.Objects;
 
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
 public class HomeActivity extends AppCompatActivity {
 
+    LinearLayout linearLayout;
     TabAdapter tabAdapter;
     TabLayout tabLayout;
     ViewPager viewPager;
+    public static final int REQUEST_AUDIO_PERMISSION_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +33,7 @@ public class HomeActivity extends AppCompatActivity {
 
         Objects.requireNonNull(getSupportActionBar()).setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.app_bar);
+        linearLayout = findViewById(R.id.linearLayout);
 
         viewPager = findViewById(R.id.viewPager);
         tabLayout = findViewById(R.id.tabLayout);
@@ -29,8 +42,42 @@ public class HomeActivity extends AppCompatActivity {
         tabAdapter.addFragment(new RecorderFragment(),"Recorder");
         tabAdapter.addFragment(new ListFragment(), "List");
 
-        viewPager.setAdapter(tabAdapter);
-        tabLayout.setupWithViewPager(viewPager);
+        if(checkPermissions()) {
+            viewPager.setAdapter(tabAdapter);
+            tabLayout.setupWithViewPager(viewPager);
+        } else {
+            requestPermissions();
+        }
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_AUDIO_PERMISSION_CODE:
+                if (grantResults.length> 0) {
+                    boolean permissionToRecord = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean permissionToStore = grantResults[1] ==  PackageManager.PERMISSION_GRANTED;
+                    if (permissionToRecord && permissionToStore) {
+                        viewPager.setAdapter(tabAdapter);
+                        tabLayout.setupWithViewPager(viewPager);
+                    } else {
+                        Snackbar.make(linearLayout,"Provide permissions to record audio",Snackbar.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+        }
+    }
+    
+    public boolean checkPermissions() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), RECORD_AUDIO);
+        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(HomeActivity.this, new String[]{RECORD_AUDIO, WRITE_EXTERNAL_STORAGE}, REQUEST_AUDIO_PERMISSION_CODE);
+    }
+
 }
+
